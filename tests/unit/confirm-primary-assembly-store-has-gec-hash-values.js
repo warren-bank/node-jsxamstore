a52d2c68100fea07f4804951f9f4fe248838cac2
Warren R. Bank <warren.r.bank@gmail.com>
2025-10-19 21:07:44 -0700

    add unit test: confirm primary assembly store has exactly "gec" hashes

---
+++ tests/unit/confirm-primary-assembly-store-has-gec-hash-values.js
@@ -0,0 +1,88 @@
+/** External Dependencies **/
+
+const fs = require('fs');
+const path = require('path');
+const { argv, exit } = require('process');
+
+/** Classes **/
+
+class AssemblyStore {
+  constructor(in_file_name) {
+    this.raw = fs.readFileSync(in_file_name);
+
+    this.hdr_lec = this.raw.readUInt32LE(8);
+    this.hdr_gec = this.raw.readUInt32LE(12);
+  }
+
+  is_first_data_offset_correct(debug) {
+    // "lec" is the count of filenames in the primary store.
+    // ("gec" - "lec") is the count of filenames in each architecture-specific store. These filenames are common to all such stores, though each filename corresponds to different data in each store.
+
+    const assert_data_offset_with_lec_hashes = 20 + (this.hdr_lec * 24) + (this.hdr_lec * 40);
+    const assert_data_offset_with_gec_hashes = 20 + (this.hdr_lec * 24) + (this.hdr_gec * 40);
+
+    const offset = 20;
+    const first_entry = this.raw.subarray(offset, offset + 24);
+    const actual_data_offset = first_entry.readUInt32LE(0);
+
+    if (debug) {
+      console.log('assert data offset with "lec" hashes:', assert_data_offset_with_lec_hashes, 'bytes')
+      console.log('assert data offset with "gec" hashes:', assert_data_offset_with_gec_hashes, 'bytes')
+      console.log('actual data offset:', actual_data_offset, 'bytes')
+    }
+
+    return (assert_data_offset_with_gec_hashes === actual_data_offset)
+  }
+}
+
+/** Utility functions **/
+
+function do_unpack(in_filename) {
+  in_filename = path.resolve(in_filename);
+
+  if (!fs.existsSync(in_filename)) {
+    console.log(`Blob '${in_filename}' does not exist!`);
+    return 1;
+  }
+
+  const assembly_store = new AssemblyStore(in_filename);
+
+  const is_correct = assembly_store.is_first_data_offset_correct(true)
+
+  console.log(is_correct ? 'OK' : 'FAIL')
+
+  return 0;
+}
+
+/** Command handlers **/
+
+function unpack_store(args) {
+  // Simple argument parsing
+  const yargs = require('yargs/yargs');
+
+  const parser = yargs(args)
+    .usage('Usage: node confirm-primary-assembly-store-has-lec-hash-values.js [options]')
+    .option('blob', {
+      alias: 'b',
+      type: 'string',
+      describe: 'Where to load primary blob from.',
+    })
+    .help(true)
+    .version(false);
+
+  const parsed_args = parser.parse();
+
+  return do_unpack(parsed_args.blob);
+}
+
+/** Main **/
+
+function main() {
+  const args = argv.slice(2);
+  return unpack_store(args);
+}
+
+if (require.main === module) {
+  const ret = main();
+  exit(ret);
+}
